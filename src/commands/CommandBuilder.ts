176248caf87103dd1a9fd310e364627f69e761c0
Quinten Degraeve <Degraeve.Quinten@gmail.com>
2022-04-03 22:40:30 +0200

    Add CommandBuilder
    - Provides easy way to define commands with redm-codelens namespace

---
+++ src/commands/CommandBuilder.ts
@@ -0,0 +1,48 @@
+import { CodeLens, Disposable, commands, Range } from 'vscode';
+
+export enum COMMAND {
+  ENABLE = 'enable',
+  DISABLE = 'disable',
+  OPEN_DOCUMENTATION = 'openDocumentation',
+  TOGGLE_NATIVE_METHOD_CODE_LENS = 'toggleNativeMethodCodeLens'
+}
+
+export enum COMMAND_TYPE {
+    DEFAULT,
+    TEXT_EDITOR
+}
+
+export const namespace = 'redm-codelens'; 
+
+type commandIdentifier = string;
+
+interface CommandConfig {
+  identifier: commandIdentifier,
+  type: COMMAND_TYPE,
+}
+
+const isCommandConfig = (command: object | string): command is CommandConfig => (command as CommandConfig).identifier !== undefined;
+
+export default class CommandBuilder {
+  
+  private readonly _namespace = namespace;
+  private _disposables: Disposable[] = [];
+  
+  constructor() { }
+
+  registerCommand(commandData: CommandConfig | commandIdentifier, callback: (...args: any[]) => any) {
+    const isTextEditorCommand = isCommandConfig(commandData);
+
+    const { registerCommand, registerTextEditorCommand } = commands;
+    const register = isTextEditorCommand ? registerTextEditorCommand : registerCommand;
+    const identifier = isTextEditorCommand ? commandData.identifier : commandData;
+
+    this._disposables.push(
+        register(`${this._namespace}.${identifier}`, callback)
+    );
+  }
+
+  getDisposables() {
+    return this._disposables;
+  }
+}
