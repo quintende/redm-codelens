769f44949a0bfdd10bad7adc6f6c636527ebdcc4
Quinten Degraeve <Degraeve.Quinten@gmail.com>
2022-04-12 18:44:19 +0200

    Add C support

--- src/selectors/nativesSelectors.ts
+++ src/selectors/nativesSelectors.ts
@@ -2,9 +2,11 @@ export const compatibleFilesSelector = [
     { language: 'typescript', scheme: 'file' },
     { language: 'javascript', scheme: 'file' },
     { language: 'lua', scheme: 'file' },
+    { language: 'c', scheme: 'file' },
     { language: 'csharp', scheme: 'file' },
     { language: 'typescript', scheme: 'untitled' },
     { language: 'javascript', scheme: 'untitled' },
     { language: 'lua', scheme: 'untitled' },
+    { language: 'c', scheme: 'untitled' },
     { language: 'csharp', scheme: 'untitled' }
 ];
