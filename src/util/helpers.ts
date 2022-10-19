4a6178151d201a6c0264cf4d4861efaf1be185cc
Quinten Degraeve <Degraeve.Quinten@gmail.com>
2022-10-19 19:31:21 +0200

    Add helper function to generate identifier

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -32,3 +32,18 @@ export const kebabCase = (string: string) => string
     .toLowerCase();
 
 export const escapeHash = (hash: string) => hash.replace(/[_'"`]+/g, '').trim();
+
+export const generateIdentifier = (document: any, match: any, hash: string) => {
+    const position = document.positionAt(match.index as number);
+    const line = document.lineAt(position.line);
+
+    let seed: string = line.text;
+    // @ts-ignore
+    seed = seed.replaceAll('()', '__');
+    seed = seed.substring(position.character);
+    seed = seed.substring(0, seed.indexOf(')'));
+
+    const identifier = kebabCase(`L:${line.lineNumber}|H:${hash}|F:${seed}`);
+    
+    return identifier;
+}
