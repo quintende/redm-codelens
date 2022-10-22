b1a5ecb66ae3ac4d186d3ca9cf7fd30c16109c91
Quinten Degraeve <Degraeve.Quinten@gmail.com>
2022-10-22 18:21:01 +0200

    Enable 'reload' and 'refetch' commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,3 +2,5 @@ import "./redmCodelens/expandAll";
 import "./redmCodelens/collapseAll";
 import "./redmCodelens/requestCollapsedStateChange";
 import "./redmCodelens/openDocumentation";
+import "./redmCodelens/refetch";
+import "./redmCodelens/reload";
